59dbd706e805abba7788ab8f4f37793bd9995953
DianaSinenkova <Diana_Sinenkova@epam.com>
2020-10-26 05:34:55 +0400

    Libraries
    Created projects with ArrayHelper and RectangleHelper

---
+++ M01/ArrayHelper/ArrayExtension.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayHelper
+{
+    public class ArrayExtension
+    {
+
+        public static T[] BubbleSortAsc<T>(T[] array) where T : IComparable<T>
+        {
+            //Тут нужно проверить на ненулевой массив
+            if (array.Length > 0)
+            {
+                T x;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        if (array[j].CompareTo(array[j]) > 0)
+                        {
+                            x = array[j - 1];
+                            array[j - 1] = array[j];
+                            array[j] = x;
+                        }
+                    }
+                }  
+            }
+            return array;
+            // а если все ж нулевой массив? 
+        }
+
+        public static T[] BubbleSortDesc<T>(T[] array) where T : IComparable<T>
+        {
+            T x;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[j].CompareTo(array[j]) < 0)
+                    {
+                        x = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = x;
+                    }
+                }
+            }
+            return array;
+        }
+
+        private static int Calculate(int[,] array)
+        {
+            int res = 0;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    if (array[i, j] > 0)
+                    {
+                        res += array[i, j];
+                    }
+
+                }
+            }
+            return res;
+        }
+
+        private static double Calculate(double[,] array)
+        {
+            double res = 0;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    if (array[i, j] > 0)
+                    {
+                        res += array[i, j];
+                    }
+                }
+            }
+            return res;
+        }
+
+        private static sbyte Calculate(sbyte[,] array)
+        {
+            sbyte res = 0;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    if (array[i, j] > 0)
+                    {
+                        res += array[i, j];
+                    }
+                }
+            }
+            return res;
+        }
+
+
+    }
+}
---
+++ M01/RectangleHelper/RectangleExtension.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace RectangleHelper
+{
+    public static class RectangleExtension
+    {
+        //int
+        public static int Perimeter(int width, int length)
+        {
+            int p = 0;
+
+            if (width > 0 && length > 0)
+            {
+                p = (2 * length) + (2 * width);
+            }
+            return p;
+        }
+
+        public static int Square(int width, int length)
+        {
+            int square = 0;
+
+            if (width > 0 && length > 0)
+            {
+                square = length * width;
+            }
+            return square;
+        }
+        //double
+        public static double Perimeter(double width, double length)
+        {
+            double p = 0;
+
+            if (width > 0 && length > 0)
+            {
+                p = (2 * length) + (2 * width);
+            }
+            return p;
+        }
+
+        public static double Square(double width, double length)
+        {
+            double square = 0;
+
+            if (width > 0 && length > 0)
+            {
+                square = length * width;
+            }
+            return square;
+        }
+
+        //float
+        public static float Perimeter(float width, float length)
+        {
+            float p = 0;
+
+            if (width > 0 && length > 0)
+            {
+                p = (2 * length) + (2 * width);
+            }
+            return p;
+        }
+
+        public static float Square(float width, float length)
+        {
+            float square = 0;
+
+            if (width > 0 && length > 0)
+            {
+                square = length * width;
+            }
+            return square;
+        }
+    }
+}
