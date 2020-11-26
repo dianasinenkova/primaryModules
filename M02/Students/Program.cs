f32581fbb0a0101a2ab19407d2b3db95949196e3
DianaSinenkova <Diana_Sinenkova@epam.com>
2020-11-26 23:29:13 +0400

    M02 Student Constructors
    Add constructors in Student
    Add Student copies

--- M02/Students/Program.cs
+++ M02/Students/Program.cs
@@ -4,9 +4,20 @@ namespace Students
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] subjects = { "Math", "PE", "English", "Literature", "Physics", "IT" };
+
+            Student st1 = new Student("alexandr.alexandrov@epam.com");
+            Student st2 = new Student("boris.borisov@epam.com");
+            Student st3 = new Student("vasiliy.vasilyev@epam.com");
+
+            Student st4 = new Student("Alexandr", "Alexandrov");
+            Student st5 = new Student("Boris", "Borisov");
+            Student st6 = new Student("Vasiliy", "Vasilyev");
+
+
+
         }
     }
 }
--- M02/Students/Student.cs
+++ M02/Students/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Students
 {
@@ -16,10 +17,22 @@ namespace Students
         public Student(string email)
         {
             Email = email;
+
+            string n = "";
+            Regex spliters = new Regex("[@.0-9]");
+            string[] name = spliters.Split(email);
+            for (int i = 0; i <= 1; i++)
+            {
+                n = name[i];
+                name[i] = char.ToUpper(n[0]) + n.Substring(1);
+            }
+            FullName = name[0] + ' ' + name[1];
+
         }
         public Student(string name, string surname)
         {
             FullName = name + ' ' + surname;
+            Email = name.ToLower() + '.' + surname.ToLower() + "@epam.com";
         }
     }
 }
